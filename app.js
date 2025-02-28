cc9a1b665f1efa5f6541969c1d8bc5e28169f578
codewithmishra-ashish <work.mishraashish@gmail.com>
2025-02-28 13:31:25 +0530

    updated arror performance

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ function toggleMenu() {
   overflow.classList.toggle("active");
 }
 
-function toggleDropdown(element) {
-  element.classList.toggle("active");
+function toggleFeaturesDropdown(element) {
+  const dropdown = element.querySelector(".dropdown");
+  const upArrow = element.querySelector(".up-arrow");
+  const downArrow = element.querySelector(".down-arrow");
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+    upArrow.style.display = "none";
+    downArrow.style.display = "inline";
+  } else {
+    dropdown.style.display = "block";
+    upArrow.style.display = "inline";
+    downArrow.style.display = "none";
+  }
+}
+
+function toggleCompanyDropdown(element) {
+  const dropdown = element.querySelector(".dropdown");
+  const upArrow = element.querySelector(".up-arrow");
+  const downArrow = element.querySelector(".down-arrow");
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+    upArrow.style.display = "none";
+    downArrow.style.display = "inline";
+  } else {
+    dropdown.style.display = "block";
+    upArrow.style.display = "inline";
+    downArrow.style.display = "none";
+  }
 }
